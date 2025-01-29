e0e086d58c40d9cbea7b988e2c24b0337bc945ec
hazamashoken <earth78203@gmail.com>
2025-01-30 01:37:16 +0800

    start working on april fool's joke

--- static/debug.js
+++ static/debug.js
@@ -3,13 +3,15 @@ document.getElementById('info-debug').innerText = 'Running in debug mode';
 
 // Make sure all elements are somewhat presentable
 const logo = document.getElementById('logo');
-logo.src = 'assets/codam.svg';
+logo.src = 'assets/logo.png';
 
 const message = document.getElementById('message');
-message.innerText = 'This is a test message that could have been sent by the back-end server for displaying.';
+message.innerText =
+  'This is a test message that could have been sent by the back-end server for displaying.';
 
 const examModeProjects = document.getElementById('exam-mode-projects');
-examModeProjects.innerText = 'Exam Rank 00, Exam Rank 01, Exam Rank 02, non-existing debug exams';
+examModeProjects.innerText =
+  'Exam Rank 00, Exam Rank 01, Exam Rank 02, non-existing debug exams';
 
 const lockedAgo = document.getElementById('active-user-session-locked-ago');
 lockedAgo.innerText = 'Automated logout occurs in 42 minutes';
@@ -30,50 +32,53 @@ const screenSwitcherContainer = document.createElement('div');
 screenSwitcherContainer.style.marginTop = '8px';
 optionsContainer.appendChild(screenSwitcherContainer);
 function switchScreen(screenId) {
-	const screens = document.querySelectorAll('main > form');
-		screens.forEach(screen => {
-			screen.style.display = 'none';
-		});
+  const screens = document.querySelectorAll('main > form');
+  screens.forEach((screen) => {
+    screen.style.display = 'none';
+  });
 
-		const selectedScreen = document.getElementById(screenId);
-		selectedScreen.style.display = 'block';
+  const selectedScreen = document.getElementById(screenId);
+  selectedScreen.style.display = 'block';
 
-		logo.style.display = (screenId === 'lock-form') ? 'none' : 'block';
+  logo.style.display = screenId === 'lock-form' ? 'none' : 'block';
 
-		// Make sure the correct input field is checked
-		const selectedInput = document.getElementById(`radio-${screenId}`);
-		selectedInput.checked = true;
+  // Make sure the correct input field is checked
+  const selectedInput = document.getElementById(`radio-${screenId}`);
+  selectedInput.checked = true;
 }
 function addScreenSwitchOption(screenName, screenId) {
-	const screenSwitcherInput = document.createElement('input');
-	screenSwitcherInput.type = 'radio';
-	screenSwitcherInput.name = 'screen';
-	screenSwitcherInput.value = screenId;
-	screenSwitcherInput.id = `radio-${screenId}`;
+  const screenSwitcherInput = document.createElement('input');
+  screenSwitcherInput.type = 'radio';
+  screenSwitcherInput.name = 'screen';
+  screenSwitcherInput.value = screenId;
+  screenSwitcherInput.id = `radio-${screenId}`;
 
-	const screenSwitcherLabel = document.createElement('label');
-	screenSwitcherLabel.textContent = screenName;
-	screenSwitcherLabel.htmlFor = `radio-${screenId}`;
-	screenSwitcherLabel.style.marginRight = '8px';
+  const screenSwitcherLabel = document.createElement('label');
+  screenSwitcherLabel.textContent = screenName;
+  screenSwitcherLabel.htmlFor = `radio-${screenId}`;
+  screenSwitcherLabel.style.marginRight = '8px';
 
-	screenSwitcherContainer.appendChild(screenSwitcherInput);
-	screenSwitcherContainer.appendChild(screenSwitcherLabel);
+  screenSwitcherContainer.appendChild(screenSwitcherInput);
+  screenSwitcherContainer.appendChild(screenSwitcherLabel);
 
-	screenSwitcherInput.addEventListener('change', () => {
-		switchScreen(screenId);
-	});
+  screenSwitcherInput.addEventListener('change', () => {
+    switchScreen(screenId);
+  });
 }
 addScreenSwitchOption('Login screen', 'login-form');
 addScreenSwitchOption('Lock screen', 'lock-form');
 addScreenSwitchOption('Exam mode', 'exam-form');
+addScreenSwitchOption('April fool', 'april-fool-form');
 switchScreen('login-form');
 
 // Add a fake calendar event from the template in HTML
 // Unfortunately these are not clickable without the proper UI toolkit
-const calendarEventTemplate = document.getElementById('intra-calendar-event-template');
+const calendarEventTemplate = document.getElementById(
+  'intra-calendar-event-template'
+);
 for (let i = 0; i < 5; i++) {
-	const calendarEvent = calendarEventTemplate.content.cloneNode(true);
-	document.getElementById('intra-calendar').appendChild(calendarEvent);
+  const calendarEvent = calendarEventTemplate.content.cloneNode(true);
+  document.getElementById('intra-calendar').appendChild(calendarEvent);
 }
 
 // Add file picker for wallpaper
@@ -82,12 +87,12 @@ wallpaperPicker.type = 'file';
 wallpaperPicker.accept = 'image/*';
 optionsContainer.appendChild(wallpaperPicker);
 wallpaperPicker.addEventListener('change', () => {
-	const file = wallpaperPicker.files[0];
-	const reader = new FileReader();
-	reader.onload = () => {
-		document.body.style.backgroundImage = `url(${reader.result})`;
-	};
-	reader.readAsDataURL(file);
+  const file = wallpaperPicker.files[0];
+  const reader = new FileReader();
+  reader.onload = () => {
+    document.body.style.backgroundImage = `url(${reader.result})`;
+  };
+  reader.readAsDataURL(file);
 });
 
 // Add slider to change the background brightness
@@ -109,5 +114,5 @@ brightnessSlider.value = '1';
 brightnessSlider.title = 'Adjust wallpaper brightness';
 optionsContainer.appendChild(brightnessSlider);
 brightnessSlider.addEventListener('input', () => {
-	brightnessFilter.style.backdropFilter = `brightness(${brightnessSlider.value})`;
+  brightnessFilter.style.backdropFilter = `brightness(${brightnessSlider.value})`;
 });
