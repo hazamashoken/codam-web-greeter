bff983a6f0df853089f05e93f2b2cb134dde7b28
hazamashoken <earth78203@gmail.com>
2025-01-30 18:52:55 +0800

    just ignore bg image check entirely

--- client/uis/wallpaper.ts
+++ client/uis/wallpaper.ts
@@ -5,8 +5,6 @@ export class WallpaperUI {
 	private _blurFilter: HTMLElement;
 	private _isLockScreen: boolean;
 
-	private _aprilFool: boolean = true;
-
 	public constructor(isLockScreen: boolean, wallpaperElement: HTMLElement | null = null) {
 		this._element = wallpaperElement ?? document.body;
 		this._blurFilter = document.getElementById('blur-filter') as HTMLElement;
@@ -24,16 +22,16 @@ export class WallpaperUI {
 			}
 		}
 
-		if (wallpaper.exists && !this._aprilFool) {
-			// Set wallpaper (yes for some reason the file path just works without file://)
-			// Actually, file:// will even cause the image to not load.
-			this._element.style.backgroundImage = 'url("' + wallpaper.path + '")';
-		}
-		else {
-			// Fall back to black color
-			this._element.style.backgroundColor = 'black';
-			this._element.style.backgroundImage = 'none';
-		}
+		// if (wallpaper.exists) {
+		// 	// Set wallpaper (yes for some reason the file path just works without file://)
+		// 	// Actually, file:// will even cause the image to not load.
+		// 	this._element.style.backgroundImage = 'url("' + wallpaper.path + '")';
+		// }
+		// else {
+		// 	// Fall back to black color
+		// 	this._element.style.backgroundColor = 'black';
+		// 	this._element.style.backgroundImage = 'none';
+		// }
 
 		return true;
 	}
