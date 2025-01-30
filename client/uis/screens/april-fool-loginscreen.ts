7ad85998c516e7dc9f130781dc6078a0bfcd7c80
hazamashoken <earth78203@gmail.com>
2025-01-30 18:19:05 +0800

    window xp don't have wiggle

--- client/uis/screens/april-fool-loginscreen.ts
+++ client/uis/screens/april-fool-loginscreen.ts
@@ -72,14 +72,14 @@ export class APFLoginScreenUI extends UIScreen {
 
   protected _wigglePasswordInput(clearInput: boolean = true): void {
     const passwordInput = (this._form as UILoginElements).passwordInput;
-    passwordInput.classList.add('wiggle');
-    passwordInput.addEventListener(
-      'keydown',
-      () => {
-        passwordInput.classList.remove('wiggle');
-      },
-      { once: true }
-    );
+    // passwordInput.classList.add('wiggle');
+    // passwordInput.addEventListener(
+    //   'keydown',
+    //   () => {
+    //     passwordInput.classList.remove('wiggle');
+    //   },
+    //   { once: true }
+    // );
 
     if (clearInput) {
       passwordInput.value = '';
